c0b02877d41eb506cea8525006dd1bd74da30c75
aashna17 <aashnakhattri2@gmail.com>
2019-03-20 09:14:19 +0530

    Added middleware and updated Readme file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # userLogin
+This is a simple User login app buit using Node.js and with help of passport.
+You can run this on terminal using "nodemon" or "npm start".
--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var createError = require('http-errors');
+//var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session=require('express-session');
 var passport=require('passport');
+var expressValidator=require('express-validator');
 var LocalStratergy=require('passport-local');
 var multer=require('multer');
+var upload=multer({dest:'./uploads'});
 var flash=require('connect-flash');
 var mongo=require('mongodb');
-var mongoose=require("mongoose");
-
-
+var mongoose=require('mongoose');
 var db=mongoose.connection;
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -22,7 +23,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 //Handle File Upload
-app.use(multer({dest:'./uploads'}));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -54,7 +55,12 @@ app.use(expressValidator({
     };
   }
 }));
-//app.use(flash);
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
